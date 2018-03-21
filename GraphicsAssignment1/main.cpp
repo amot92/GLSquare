d2046b7adf830c96ac540f9fdf0a2813459b693b
Adam Oakes <amot92@mail.missouri.edu>
2018-03-21 11:50:04 -0500

    Initial commit

--- GraphicsAssignment1/main.cpp
+++ GraphicsAssignment1/main.cpp
@@ -1,15 +1,174 @@
-//
-//  main.cpp
-//  GraphicsAssignment1
-//
-//  Created by Adam Oakes on 2/11/18.
-//  Copyright © 2018 Adam Oakes. All rights reserved.
-//
+// Simple.cpp
+// Generate a square on a solid background
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "GL/glew.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "GLFW/glfw3.h"
+#include "Angel.h"
+
+typedef vec4 point4;
+typedef  vec4  color4;
+
+GLuint shader_programme;
+GLuint vao;
+GLint col;
+int fred = 0;
+//----------------------------------------------------------------------------
+
+color4 vertex_colors[8] = {
+    color4(1.0, 0.0, 0.0, 1.0),  // red
+     color4(0.0, 0.0, 0.0, 1.0),  // black
+    color4(1.0, 1.0, 0.0, 1.0),  // yellow
+    color4(0.0, 1.0, 0.0, 1.0),  // green
+    color4(0.0, 0.0, 1.0, 1.0),  // blue
+    color4(1.0, 0.0, 1.0, 1.0),  // magenta
+    color4(1.0, 1.0, 1.0, 1.0),  // white
+    color4(0.0, 1.0, 1.0, 1.0)   // cyan
+};
+
+void init( void )
+{
+    
+    GLuint vert_shader, frag_shader;
+    /* GL shader programme object [combined, to link] */
+    
+    // Specifiy the vertices for a triangle
+    GLfloat points[] = { -0.5, 0.5, //top left
+                        0.5, 0.5,   //top right
+                        0.5, -0.5,  //bottom right
+                         -0.5, 0.5,//top left
+                        -0.5, -0.5,//bottom left
+                        0.5, -0.5 };//bottom right
+
+    
+    // Create and initialize a buffer object
+    GLuint buffer;
+    glGenBuffers( 1, &buffer );
+    glBindBuffer( GL_ARRAY_BUFFER, buffer );
+    glBufferData( GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW );
+    
+    // Create a vertex array object
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    
+    // "attribute #0 is created from every 2 variables in the above buffer, of type
+    // float (i.e. make me vec2s)"
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+    
+    // Load shaders and use the resulting shader program
+    const char *vertex_shader = "#version 410\n"
+    "in vec4 vPosition;"
+    "void main () {"
+    "    gl_Position = vPosition;"
+    "}";
+    /* the fragment shader colours each fragment (pixel-sized area of the
+     triangle) */
+    const char *fragment_shader = "#version 410\n"
+    "uniform vec4 color = vec4( 0.478, 0.729, 0.443, 1.0 );"
+    "out vec4 FragColor;"
+    "void main () {"
+//    "    FragColor = vec4( 0.478, 0.729, 0.443, 1.0 );"
+    "    FragColor = color;"
+    "}";
+    
+    vert_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert_shader, 1, &vertex_shader, NULL);
+    glCompileShader(vert_shader);
+    frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, &fragment_shader, NULL);
+    glCompileShader(frag_shader);
+    shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, frag_shader);
+    glAttachShader(shader_programme, vert_shader);
+    glLinkProgram(shader_programme);
+    
+    
+}
+
+void mymouse(GLFWwindow* window, int button, int action, int mods){
+    if (GLFW_PRESS == action && button == GLFW_MOUSE_BUTTON_RIGHT){
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void mykey(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+        vec4 m = vertex_colors[fred];
+        glUniform4fv(col, 1, m);
+        fred++;
+        if(fred == 8){
+            fred = 0;
+        }
+    }
+}
+
+
+//----------------------------------------------------------------------------
+
+int main( int argc, char **argv )
+{
+    GLFWwindow *window = NULL;
+    const GLubyte *renderer;
+    const GLubyte *version;
+    /* start GL context and O/S window using the GLFW helper library */
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        return 1;
+    }
+    
+    /* We must specify 3.2 core if on Apple OS X -- other O/S can specify
+     anything here. I defined 'APPLE' in the makefile for OS X
+     
+     Remove the #ifdef #endif and play around with this - you should be starting
+     an explicit version anyway, and some non-Apple drivers will require this too.
+     */
+    //#ifdef APPLE
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //#endif
+    window = glfwCreateWindow(640, 640, "simple", NULL, NULL);
+    
+    if (!window) {
+        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    /* start GLEW extension handler */
+    glewExperimental = GL_TRUE;
+    glewInit();
+    renderer = glGetString(GL_RENDERER); /* get renderer string */
+    version = glGetString(GL_VERSION);     /* version as a string */
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+    init();
+    
+    glfwSetMouseButtonCallback(window, mymouse);
+    glfwSetKeyCallback(window, mykey);
+    
+    col = glGetUniformLocation(shader_programme, "color"); //in main function
+    
+    
+    do{
+        glClear(GL_COLOR_BUFFER_BIT);     // clear the window
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6 );    // draw the points
+        /* update other events like input handling */
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    while (!glfwWindowShouldClose(window));
+    
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
     return 0;
+    
 }
